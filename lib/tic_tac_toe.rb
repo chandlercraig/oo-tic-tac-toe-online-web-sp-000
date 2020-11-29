910fd17a3d13b02fd7cc990a5feb65537a1a6e82
Chandler Craig <hellochandlercraig@gmail.com>
2020-11-29 17:37:20 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -36,8 +36,9 @@ class TicTacToe
     @board[index] == "X" || @board[index]=="O"
   end
 
-  def valid_move?
-
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(index)
   end
+
 
 end

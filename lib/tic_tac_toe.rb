0d12da4219e3a3f62b402dd172f7f2a5dedd6719
Chandler Craig <hellochandlercraig@gmail.com>
2020-11-29 17:23:43 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -35,6 +35,7 @@ class TicTacToe
   def position_taken?(int)
     index = input_to_index(int)
     !@board[index] == " " || !@board[index] == nil
+    board[index]== "X" || board[index]=="O"
   end
 
 
